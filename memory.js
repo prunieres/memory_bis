476a394dcf66ef4d8c9d136264f38144124fdba6
PRUNIERES <aurelie.prunieres@gmail.com>
2018-06-11 15:30:07 +0200

    visible invisible

--- memory.js
+++ memory.js
@@ -1,23 +1,10 @@
 var cards = document.getElementsByClassName('card');
 var imgs = document.getElementsByTagName('img');
-// console.log(cards);
-// console.log(cards);
 
 for (i=0; i< cards.length; i++){
     cards[i].addEventListener("click", display);
+    function display(){
+    	this.classList.toggle('invisible');
+    }
 }
 
-function display(){
-	this.classList.add('invisible');
-	if (this.className = 'invisible'){
-		this.className = 'visible';
-	}else{
-		this.classList.add('invisible');
-	}
-}
-   
-		// if(this.className = 'invisible'){
-		// 	this.className = 'visible';
-		// }else{
-		// 	this.className = 'invisible';
-		// }
