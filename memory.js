90d5fea46ae8c0b47e11dd1674d9e34ee3453603
PRUNIERES <aurelie.prunieres@gmail.com>
2018-06-11 13:37:17 +0200

    alout classes visible invisible

--- memory.js
+++ memory.js
@@ -1,4 +1,23 @@
-var imgs = document.getElementsByTagName('img');
 var cards = document.getElementsByClassName('card');
+var imgs = document.getElementsByTagName('img');
 // console.log(cards);
-console.log(cards);
+// console.log(cards);
+
+for (i=0; i< cards.length; i++){
+    cards[i].addEventListener("click", display);
+}
+
+function display(){
+	this.classList.add('invisible');
+	if (this.className = 'invisible'){
+		this.className = 'visible';
+	}else{
+		this.classList.add('invisible');
+	}
+}
+   
+		// if(this.className = 'invisible'){
+		// 	this.className = 'visible';
+		// }else{
+		// 	this.className = 'invisible';
+		// }
